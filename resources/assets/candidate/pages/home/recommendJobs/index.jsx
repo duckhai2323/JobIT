424dc7e16cbdefec45b25d0e7fe20f70e6772059
khaitd0452 <tranduckhai26112003@gmail.com>
2024-10-29 04:24:06 +0700

    #task25-Refactor UI - Create Page Recommend Role Candidate

--- resources/assets/candidate/pages/home/recommendJobs/index.jsx
+++ resources/assets/candidate/pages/home/recommendJobs/index.jsx
@@ -3,16 +3,25 @@ import style from './recommendJobs.module.scss';
 import classNames from 'classnames/bind';
 import JobItemRecommend from './jobItemRecommend';
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+import { useNavigate } from 'react-router-dom';
 
 const cx = classNames.bind(style);
 const RecommendJobs = () => {
+  const navigate = useNavigate();
   return (
     <div className={cx('section-recommend-jobs')}>
       <div className={cx('recommend-jobs-group')}>
         <div className={cx('recommend-jobs-group__title')}>
           <span>Gợi ý việc làm phù hợp</span>
           <div className={cx('arrow-button-group')}>
-            <span className={cx('see-all')}>Xem tất cả</span>
+            <span
+              className={cx('see-all')}
+              onClick={() => {
+                navigate('/recommend');
+              }}
+            >
+              Xem tất cả
+            </span>
             <div className={cx('arrow-button')}>
               <IoIosArrowBack className={cx('icon-arrow')} />
             </div>
--- resources/assets/candidate/pages/recommend/index.jsx
+++ resources/assets/candidate/pages/recommend/index.jsx
@@ -1,19 +1,21 @@
 import LayoutCandidate from '@/candidate/components/layout';
 import React from 'react';
 import HeaderSearchJobDetail from '../../components/header_search';
-import SectionJobInfor from './section_infor';
-import SectionContent from './section_content';
+import style from './recommend.module.scss';
+import classNames from 'classnames/bind';
+import RecommendContentJob from './recommend_job_content';
 import FooterHome from '../home/footer';
 
-const JobDetailsPageCandidate = () => {
+const cx = classNames.bind(style);
+
+const RecommendJobPage = () => {
   return (
     <LayoutCandidate>
       <HeaderSearchJobDetail />
-      <SectionJobInfor />
-      <SectionContent />
+      <RecommendContentJob />
       <FooterHome background={'white'} />
     </LayoutCandidate>
   );
 };
 
-export default JobDetailsPageCandidate;
+export default RecommendJobPage;
